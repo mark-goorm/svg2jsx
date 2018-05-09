96cbc733f7938d393babeeb4bad9a4b9d65768ed
minung.han <minung.han@goorm.io>
2018-05-09 05:48:01 +0000

    [Add] template

--- index.js
+++ index.js
@@ -2,12 +2,52 @@ const fs = require('fs');
 const path = require('path');
 const svg2jsx = require('@balajmarius/svg2jsx');
 
+const template = ({ fileName, svgData }) => {
+  return `import React from 'react';
+import PropTypes from 'prop-types';
+import IconBase from 'react-icon-base';
+
+function ${fileName}(props) {
+  return (
+    ${svgData}
+  );
+}
+
+${fileName}.defaultProps = {
+  size: '1rem',
+  color: 'currentColor',
+  onClick: () => {},
+  style: {},
+};
+
+${fileName}.propTypes = {
+  size: PropTypes.string,
+  color: PropTypes.string,
+  onClick: PropTypes.func,
+  style: PropTypes.objectOf(PropTypes.string),
+};
+
+export default ${fileName};`;
+};
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 fs.readdir('./images', (err, files) => {
-  files.forEach(async (filePath) => {
+  files.forEach(async filePath => {
     const fileName = filePath.split('.')[0];
     const data = fs.readFileSync(`./images/${filePath}`, 'utf8');
     const transformedSVG = await svg2jsx(data);
-    fs.writeFileSync(`./results/${fileName}.jsx`, transformedSVG.replace('<svg', '<IconBase {...props}').replace('</svg>', '</IconBase>'));
+    fs.writeFileSync(
+      `./results/${fileName}.jsx`,
+      template({
+        fileName: capitalizeFirstLetter(fileName),
+        svgData: transformedSVG
+          .replace('<svg', '<IconBase {...props}')
+          .replace('</svg>', '</IconBase>')
+      })
+    );
     console.log(filePath);
   });
-})
+});
